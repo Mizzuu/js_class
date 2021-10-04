66dd6f7bdd3dd74dae38099f436f208ecf041356
Mizzoo <87053514+Mizzuu@users.noreply.github.com>
2021-10-03 18:06:16 -0700

    second commit

--- js/main.js
+++ js/main.js
@@ -14,13 +14,54 @@ submit.addEventListener('click', () => {
     console.log(nut1);
     foodArr.push(nut1);
     console.log(foodArr);
+    
+    // CARB
+    let carbLargest = 0;
+    let carbMax;
+    for(let i = 0; i < foodArr.length; i++) {
+        if(foodArr[i].carb > carbLargest) {
+            carbLargest = foodArr[i].carb;
+            carbMax = foodArr[i].food;
+        }
+    }
+    console.log(carbLargest);
+    console.log(carbMax);
 
+    // FAT
+    let fatLargest = 0;
+    let fatMax;
+    for(let i = 0; i < foodArr.length; i++) {
+        if(foodArr[i].fat > fatLargest) {
+            fatLargest = foodArr[i].fat;
+            fatMax = foodArr[i].food;
+        }
+    }
+    console.log(fatLargest);
+    console.log(fatMax);
+
+    // PROTEIN
+    let proLargest = 0;
+    let proMax;
+    for(let i = 0; i < foodArr.length; i++) {
+        if(foodArr[i].protein > proLargest) {
+            proLargest = foodArr[i].protein;
+            proMax = foodArr[i].food;
+        }
+    }
+    console.log(proLargest);
+    console.log(proMax);
+
+    
     food.value = "";
     carb.value = "";
     fat.value = "";
     protein.value = "";
+    food.focus();
+
+    output.innerHTML = ` 
+        ${ carbMax } has highest carb<br>
+        ${ fatMax } has highest fat<br>
+        ${ proMax } has highest protein`
+        
 })
 
-// output.innerHTML = ;
-
-// uuhihi
